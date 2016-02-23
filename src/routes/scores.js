cb1fe8b205d1dd3f1161fbf089bf8bf3cae28910
Marc Solà Nadal <ionthasMail@gmail.com>
2016-02-23 12:27:27 +0100

    Added Redis container configuration and toying with ordered sets

--- src/routes/scores.js
+++ src/routes/scores.js
@@ -1,31 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-var redis = require('../db.js');
+var rcli = require('../db.js');
 
 // middleware that is specific to this router
 
+// Checking if the user is logged in
 router.use(function (req, res, next) {
   console.log('checking if user has permissions to use scores');
-  next();
-});
-
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
   next();
 });
 
 // middleware routes
 
 router.get('/', function (req, res) {
-  var score = rcli.get('score');
-  res.send('GET score: ' + score);
+  var score = rcli.get('score', function(err, data) {
+    res.send('score: ' + data);
+  });
 });
 
 router.post('/', function (req, res) {
+  //rcli.zadd(user, function(err, data) {
   var score = req.body.score;
   rcli.set('score', score);
-  console.log('score: ' + score);
   res.send('POST score ' + score);
 });
 
